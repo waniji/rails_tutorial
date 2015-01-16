5b2c067798ca33b3893bb0bcf8306bdd8f301795
Makoto Sasaki <wanigoya@gmail.com>
2015-01-16 11:19:06 +0900

    Finish updating deleteting exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :not_signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :signed_in_user, only: [:new, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -50,11 +51,15 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def signed_in_user
+  def not_signed_in_user
     unless signed_in?
       store_location
       redirect_to signin_url, notice: "Please sign in."
     end
+  end
+
+  def signed_in_user
+    redirect_to(root_path) if signed_in?
   end
 
   def correct_user
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -10,6 +10,10 @@ describe "Authentication" do
     describe "with invalid information" do
       before { click_button "Sign in" }
 
+      it { should_not have_link('Users') }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out') }
       it { should have_title('Sign in') }
       it { should have_error_message('Invalid') }
 
@@ -39,6 +43,24 @@ describe "Authentication" do
 
   describe "authorization" do
 
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#new action" do
+        before { get signup_path }
+        specify { expect(response.body).not_to match(full_title('Sign up')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "submitting a POST request to the Users#create action" do
+        before { post users_path(user) }
+        specify { expect(response.body).not_to match(full_title('Sign up')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+    end
+
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
@@ -63,7 +85,7 @@ describe "Authentication" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email", with: user.email
+          fill_in "Email",    with: user.email
           fill_in "Password", with: user.password
           click_button "Sign in"
         end
@@ -71,6 +93,17 @@ describe "Authentication" do
         describe "after signing in" do
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
