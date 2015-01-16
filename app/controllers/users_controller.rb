4eb6c6054b0c2e344cfee4e42eab9bfbdb597bc5
Makoto Sasaki <wanigoya@gmail.com>
2015-01-16 16:27:10 +0900

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :not_signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :signed_in_user, only: [:new, :create]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :created_user, only: [:new, :create]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
 
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -51,14 +52,7 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 
-  def not_signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
-  end
-
-  def signed_in_user
+  def created_user
     redirect_to(root_path) if signed_in?
   end
 
