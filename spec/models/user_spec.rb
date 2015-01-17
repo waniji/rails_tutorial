10e46bac605bca77c00fb654c870e87117a39697
Makoto Sasaki <wanigoya@gmail.com>
2015-01-18 01:31:17 +0900

    Add user following

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,13 @@ describe User do
   it { should respond_to(:authenticate) }
   it { should respond_to(:admin) }
   it { should respond_to(:microposts) }
+  it { should respond_to(:feed) }
+  it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:reverse_relationships) }
+  it { should respond_to(:followers) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
 
   it { should be_valid }
   it { should_not be_admin }
@@ -151,10 +158,44 @@ describe User do
       let(:unfollowed_post) do
         FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
       end
+      let(:followed_user) { FactoryGirl.create(:user) }
+
+      before do
+        @user.follow!(followed_user)
+        3.times { followed_user.microposts.create!(content: "Lorem ipsum") }
+      end
 
       it { expect(@user.feed).to include(newer_micropost) }
       it { expect(@user.feed).to include(older_micropost) }
       it { expect(@user.feed).not_to include(unfollowed_post) }
+      it do
+        followed_user.microposts.each do |micropost|
+          expect(@user.feed).to include(micropost)
+        end
+      end
+    end
+
+    describe "following" do
+      let(:other_user) { FactoryGirl.create(:user) }
+      before do
+        @user.save
+        @user.follow!(other_user)
+      end
+
+      it { should be_following(other_user) }
+      it { expect(@user.followed_users).to include(other_user) }
+
+      describe "followed user" do
+        subject { other_user.followers }
+        it { should include(@user) }
+      end
+
+      describe "and unfollowing" do
+        before { @user.unfollow!(other_user) }
+
+        it { should_not be_following(other_user) }
+        it { expect(@user.followed_users).not_to include(other_user) }
+      end
     end
   end
 end
